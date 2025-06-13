ff49aedec37795b6f327dd0deb78ff2b14f62117
kapil thakur <kapil.thakur@pos.com.my>
2025-06-13 18:52:30 +0530

    docker file

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 
 
  //const MONGO_URL="mongodb+srv://ktkapilth:30oct1988@zapd.j1x9h.mongodb.net/"
- const MONGO_URL="mongodb://admin:root@example:27017";
+ const MONGO_URL="mongodb://root:example@mongo:27017/";
  module.exports=MONGO_URL
