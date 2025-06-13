d2ab93c54a7d490bacf82dadbeee1254bf19a478
kapil thakur <kapil.thakur@pos.com.my>
2025-06-13 19:51:10 +0530

    docker file

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,6 @@
 
 
  //const MONGO_URL="mongodb+srv://ktkapilth:30oct1988@zapd.j1x9h.mongodb.net/"
- const MONGO_URL="mongodb://mongo:27017/zapd";
+ const MONGO_URL="mongodb://root:example@localhost:27017";
  module.exports=MONGO_URL
+ //18 19 23
--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,14 +1,11 @@
 const mongoose=require("mongoose")
 const MONGO_URL =require("./config")
-// mongoose.connect(MONGO_URL,{
-//    // useNewUrlParser:true,
-//    // useUnifiedTopolgy:true
-// });
-
-mongoose.connect(MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(MONGO_URL,{
+   // useNewUrlParser:true,
+   // useUnifiedTopolgy:true
 });
+
+
 mongoose.connection.on("connected",()=>{
    console.log( "Mongo db connected successfully");
 })
