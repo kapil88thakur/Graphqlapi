98f21536aa751cd1a7f34b78f6dc0155ab325055
kapil thakur <kapil.thakur@pos.com.my>
2025-06-17 15:22:13 +0530

    docker file

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -3,7 +3,7 @@ const MONGO_URL =require("./config")
 mongoose.connect(MONGO_URL,{
     //useNewUrlParser:true,
      //useUnifiedTopolgy:true
-}).then(() => console.log('✅ Connected to MongoDB'))
+}).then(() => console.log('✅ Connected to MongoDB kapil'))
 .catch(err => console.error('❌ MongoDB connection error:', err));
 
 
