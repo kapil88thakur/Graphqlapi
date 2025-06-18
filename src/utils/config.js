6e09c91577b6d8193aed4b7e83977a677798124a
kapil thakur <kapil.thakur@pos.com.my>
2025-06-18 11:34:59 +0530

    docker file

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 
  //const MONGO_URL="mongodb://root:example@mongo:27017/zapd?authSource=admin"
   //const MONGO_URL="mongodb+srv://ktkapilth:30oct1988@zapd.j1x9h.mongodb.net/";
- const MONGO_URL=process.env['MONGO_URI'];
+ const MONGO_URL=process.env['MONGO_URI']?process.env['MONGO_URI']:'mongodb+srv://ktkapilth:30oct1988@zapd.j1x9h.mongodb.net/';
  module.exports=MONGO_URL
  //18 19 23
