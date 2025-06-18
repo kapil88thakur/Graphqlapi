4d2e6ca35ed36e3888decfda48320717ab7e5d79
kapil thakur <kapil.thakur@pos.com.my>
2025-06-18 16:25:46 +0530

    docker file

--- src/server.js
+++ src/server.js
@@ -21,5 +21,5 @@ const server= new ApolloServer(
 
 server.listen().then(({url})=>{
 
-    console.log(`1 Kapil thakur server is ready at ${url}`)
+    console.log(`15665 Kapil thakur server is ready at ${url}`)
 })
